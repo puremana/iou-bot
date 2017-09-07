3c5a73412d3a69b794392f544c7a1a8fcc5ae190
Jeremy Shaw <puremana@gmail.com>
2017-09-07 23:39:42 +1200

    Converted to case/switch format.

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 var config = require('./storage/config.json');
 const TOKEN = config.token;
-const botName = "IOU Bot";
-const prefix = "?";
+const BOTNAME = "IOU Bot";
+const PREFIX = "?";
 
 bot.on("ready", function() {
 	console.log("Bot ready...")
@@ -14,61 +14,61 @@ bot.on("message", function(message) {
 	if (message.author.equals(bot.user)) {
 		return;
 	}
-	
-	if (message.channel.id == "355250699841568768") {
-		if (message.content == "?bronze") {
-			message.channel.sendMessage("http://i.imgur.com/POra9Kx.jpg");
-		}
-		else if (message.content == "?silver") {
-			message.channel.sendMessage("http://i.imgur.com/rkJ51fC.jpg");
-		}
-		else if (message.content == "?gold") {
-			message.channel.sendMessage("http://i.imgur.com/5GXghiA.jpg");
-		}
-	}
-	
-	if (message.content == "?invasion") {
-		message.channel.sendMessage("https://docs.google.com/spreadsheets/d/1RDw0FEdFd6lKhmvMK972J5_xvvjVYeUYrTQQ4ZbMR74/edit");
-	}
-	
-	
-	
-	if (message.content == "?help") {
-		//Challenge Commands
-		var help = "**Challenge Commands** \n" +
-		"?bronze \n" +
-		"?silver \n" +
-		"?gold \n" +
-		//Event Commands
-		"**Event Commands** \n" +
-		"?invasion \n" + 
-		//Other Commands
-		"**Other Commands** \n" + 
-		"?help \n" + 
-		"?info \n" +
-		"?suggest \n" +
-		"?guide \n";
-		message.author.send(help);
-	}
-	
-	if (message.content == ("?info")) {
-		var info = "IOU Bot is made with love (and nodejs) by Level \n" + 
-		"Check out the source code at https://github.com/puremana/iou-bot";
-		message.channel.sendMessage(info);
-	}
-	
-	if (message.content == ("?suggest")) {
-		message.channel.sendMessage("If you want something added, post an issue at https://github.com/puremana/iou-bot/issues");
-	}
-	
-	if (message.content == "?guide") {
-		message.channel.sendMessage("https://tinyurl.com/IOUguide");
-	}
-	
-	if (message.content == "?guide") {
-		message.channel.sendMessage("https://tinyurl.com/IOUguide");
-	}
-	
+    
+    if (!message.content.startsWith(PREFIX)) {
+        return;
+    }
+    
+    var args = message.content.substring(PREFIX.length).split(" ");
+    
+    switch (args[0].toLowerCase()) {
+        //Challenge Commands
+        case "bronze":       
+            message.channel.send("http://i.imgur.com/POra9Kx.jpg");
+            break;
+        case "silver":
+            message.channel.send("http://i.imgur.com/rkJ51fC.jpg");
+            break;
+        case "gold":
+            message.channel.send("http://i.imgur.com/5GXghiA.jpg");
+            break;
+        //Event Commands
+        case "invasion":
+            message.channel.send("https://docs.google.com/spreadsheets/d/1RDw0FEdFd6lKhmvMK972J5_xvvjVYeUYrTQQ4ZbMR74/edit");
+            break;
+        case "energyevent":
+            message.channel.send("https://docs.google.com/spreadsheets/d/1R97uuDvEI80LBbqxveXIyHbwMGXG-nZsGvlH5YNoiV8/edit");
+            break;
+        case "rpg": 
+            message.channel.send("")
+            break;
+        
+        //Bot Related Commands
+        case "help":
+            var help = "**Bot Related Commands** \n" +
+            "?help \n" + 
+            "?info \n" +
+            "?suggest \n" +
+            "**Challenge Commands** \n" +
+            "?bronze \n" +
+            "?silver \n" +
+            "?gold \n" +
+            "**Event Commands** \n" +
+            "?invasion \n" + 
+            "**Useful Links** \n" + 
+            "?guide \n"
+            "**Other Commands** \n" +
+            "";
+            message.author.send(help);
+            break;
+        case "info":
+            
+            break;
+        //Useful Links
+        case "guide":
+            message.channel.send("https://tinyurl.com/IOUguide");
+            break;
+    }
 });
-
+       
 bot.login(TOKEN);
